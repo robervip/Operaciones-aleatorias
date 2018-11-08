dee03e98d0d7c618b89d532efcd8d1a0b329e252
robervip <44229332+robervip@users.noreply.github.com>
2018-11-08 17:25:49 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -3,23 +3,22 @@ var signo=["+","-"];
 var numero2=[];
 var aleatorio1=0;
 var aleatorio2=0;
-var signoAleatorio=[];
+var signoAleatorio="";
 var pos=0;
-var i=0;
 var resultado=0;
 function numAleatorios() {
   aleatorio1=Math.floor(Math.random(numero1) * 10 + 1);
-  signoAleatorio=Math.floor(Math.random(signo)*(1-0+1));
+  signoAleatorio=Math.floor(Math.random(signo)*(1+1)+0);
   aleatorio2=Math.floor(Math.random(numero1) * 10 + 1);
-  i=signoAleatorio;
-  signoAleatorio[i];
-  pos=i;
+  pos=signoAleatorio;
+  signo[pos];
   resultado=(prompt(`${aleatorio1}${signo[pos]}${aleatorio2}=`));
   console.log(`Tú respuesta es ${resultado}.`);
-    if (signoAleatorio=0) {
+    if (signo[pos]!="-") {
       console.log(`La respuesta correcta es ${eval(aleatorio1+aleatorio2)}.`);
+      return;
   }
-    if (signoAleatorio=1) {
+    if (signo[pos]!="+") {
       console.log(`La respuesta correcta es ${eval(aleatorio1-aleatorio2)}.`);
   }
   }
